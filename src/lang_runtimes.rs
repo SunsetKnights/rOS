a893fb2631da5c80f4f1757ef00b0a6d18ca7a63
SunsetKnights <sunsetknightss@gmail.com>
2023-11-08 17:58:10 +0800

    通过已实现的println宏打印错误信息并关机

--- src/lang_runtimes.rs
+++ src/lang_runtimes.rs
@@ -1,6 +1,17 @@
+use crate::{println, sbi_services::shutdown};
 use core::panic::PanicInfo;
 
 #[panic_handler]
-fn panic(_info: &PanicInfo) -> ! {
-    loop {}
+fn panic(info: &PanicInfo) -> ! {
+    if let Some(location) = info.location() {
+        println!(
+            "Panicked at {}:{}\n{}",
+            location.file(),
+            location.line(),
+            info.message().unwrap()
+        );
+    } else {
+        println!("Panicked: {}", info.message().unwrap());
+    }
+    shutdown()
 }
