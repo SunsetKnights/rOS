9d7cbe33c86253f118b3e83fdc5ced6edbcef3c1
SunsetKnights <sunsetknightss@gmail.com>
2023-11-08 18:28:54 +0800

    使用了最新的sbi接口调用规范

--- src/sbi_services.rs
+++ src/sbi_services.rs
@@ -9,14 +9,16 @@ const SBI_SEND_IPI: usize = 4;
 const SBI_REMOTE_FENCE_I: usize = 5;
 const SBI_REMOTE_FENCE_VMA: usize = 6;
 const SBI_REMOTE_FENCE_VMA_ASID: usize = 7;
-const SBI_SHUTDOWN: usize = 8;
+
+const SRST_EXTENSION: usize = 0x53525354;
+const SBI_SHUTDOWN: usize = 0;
 
 use core::arch::asm;
 
 #[inline(always)]
 // 一个调用sbi服务的函数
 // service_type表示请求服务的i类型，arg0-arg2表示三个参数，返回值会被放入arg0中
-fn sbi_call(service_type: usize, arg0: usize, arg1: usize, arg2: usize) -> usize {
+fn sbi_call(service_type: usize, fid: usize, arg0: usize, arg1: usize, arg2: usize) -> usize {
     let mut result;
     unsafe {
         asm! {
@@ -24,6 +26,7 @@ fn sbi_call(service_type: usize, arg0: usize, arg1: usize, arg2: usize) -> usize
             inlateout("x10") arg0=>result,
             in("x11") arg1,
             in("x12") arg2,
+            in("x16") fid,
             in("x17") service_type,
         }
     }
@@ -32,11 +35,11 @@ fn sbi_call(service_type: usize, arg0: usize, arg1: usize, arg2: usize) -> usize
 
 // 在屏幕上输出一个字符
 pub fn console_putchar(c: usize) {
-    sbi_call(SBI_CONSOLE_PUTCHAR, c, 0, 0);
+    sbi_call(SBI_CONSOLE_PUTCHAR, 0, c, 0, 0);
 }
 
 // 关机
 pub fn shutdown() -> ! {
-    sbi_call(SBI_SHUTDOWN, 0, 0, 0);
+    sbi_call(SRST_EXTENSION, SBI_SHUTDOWN, 0, 0, 0);
     panic!("shutdown command executed!");
 }
