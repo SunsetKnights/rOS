6633a197b1732dd156e6c4120e5583e218248d92
SunsetKnights <sunsetknightss@gmail.com>
2023-11-17 15:27:09 +0800

    文件相关的系统调用

---
+++ OS-Core/src/syscall/fs.rs
@@ -0,0 +1,27 @@
+// File stream mod
+
+// file descriptor 1: standard output stream
+const FD_STDOUT: usize = 1;
+
+/// wtire buf of lenth "len" to file fd
+///
+/// # parameter
+/// * 'fd' - file descriptor
+/// * 'buf' - a pointer to the content to be written
+/// * 'len' -  the length of the content to be written
+///
+/// # return
+/// the length of the fd successfully written
+pub fn sys_write(fd: usize, buf: *const u8, len: usize) -> isize {
+    match fd {
+        FD_STDOUT => {
+            let slice = unsafe { core::slice::from_raw_parts(buf, len) };
+            let str = core::str::from_utf8(slice).unwrap();
+            print!(str);
+            len as isize
+        }
+        _ => {
+            panic!("unsupported fd in sys_write!");
+        }
+    }
+}
