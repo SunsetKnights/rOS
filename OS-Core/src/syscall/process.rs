a8d3392a8a720f4be9c70dd0a017fd7fcaf48820
SunsetKnights <sunsetknightss@gmail.com>
2023-11-17 15:27:35 +0800

    进程管理相关的系统调用

---
+++ OS-Core/src/syscall/process.rs
@@ -0,0 +1,7 @@
+// process manage mod
+
+pub fn sys_exit(exit_code: i32) -> ! {
+    println!("[kernel] Application exited with code {}", exit_code);
+    // here need that run next app function
+    todo!()
+}
