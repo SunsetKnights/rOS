383d3890d75681b756be635ee394d1b1da992350
SunsetKnights <sunsetknightss@gmail.com>
2023-11-17 15:39:06 +0800

    批处理系统的主要逻辑

---
+++ OS-Core/src/batch.rs
@@ -0,0 +1,50 @@
+use crate::trap::TrapContext;
+
+const USER_STACK_SIZE: usize = 4096 * 2;
+const KERNEL_STACK_SIZE: usize = 4096 * 2;
+const MAX_APP_NUM: usize = 16;
+const APP_BASE_ADDRESS: usize = 0x80400000;
+const APP_SIZE_LIMIT: usize = 0x20000;
+
+#[repr(align(4096))]
+struct KernelStack {
+    data: [u8; KERNEL_STACK_SIZE],
+}
+
+#[repr(align(4096))]
+struct UserStack {
+    data: [u8; USER_STACK_SIZE],
+}
+
+static KERNEL_STACK: KernelStack = KernelStack {
+    data: [0; KERNEL_STACK_SIZE],
+};
+static USER_STACK: UserStack = UserStack {
+    data: [0; KERNEL_STACK_SIZE],
+};
+
+impl KernelStack {
+    fn get_sp(&self) -> usize {
+        self.data.as_ptr() as usize + KERNEL_STACK_SIZE
+    }
+    pub fn push_context(&self, context: TrapContext) -> &'static mut TrapContext {
+        let context_ptr = (self.get_sp() - core::mem::size_of::<TrapContext>()) as *mut TrapContext;
+        unsafe {
+            *context_ptr = context;
+        }
+        unsafe { context_ptr.as_mut().unwrap() }
+    }
+}
+
+impl UserStack {
+    fn get_sp(&self) -> usize {
+        self.data.as_ptr() as usize + USER_STACK_SIZE
+    }
+}
+
+struct AppManager {
+    num_app: usize,
+    current_app: usize,
+    // why MAX_APP_NUM plus 1??????
+    app_start: [usize; MAX_APP_NUM + 1],
+}
