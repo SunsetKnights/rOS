fcba823a119d5ebba140b9893d301015d62fc1da
SunsetKnights <sunsetknightss@gmail.com>
2023-11-24 21:00:58 +0800

    system config

---
+++ OS-Core/src/config.rs
@@ -0,0 +1,13 @@
+// All system constants
+
+pub const USER_STACK_SIZE: usize = 4096 * 2;
+pub const KERNEL_STACK_SIZE: usize = 4096 * 2;
+pub const MAX_APP_NUM: usize = 4;
+pub const APP_BASE_ADDRESS: usize = 0x80400000;
+pub const APP_SIZE_LIMIT: usize = 0x20000;
+
+pub const CLOCK_FREQ: usize = 12500000;
+pub const TICKS_PER_SEC: usize = 100;
+
+pub const SYSCALL_QUANTITY: usize = 5;
+pub const SYSCALL_ID: [usize; SYSCALL_QUANTITY] = [64, 93, 124, 169, 410];
