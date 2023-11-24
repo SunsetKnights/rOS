0d97a1a6bb485e667776575dead34df5595c9daf
SunsetKnights <sunsetknightss@gmail.com>
2023-11-24 21:03:33 +0800

    support use time register

---
+++ OS-Core/src/timer.rs
@@ -0,0 +1,15 @@
+use crate::config::{CLOCK_FREQ, TICKS_PER_SEC};
+use crate::sbi_services::set_timer;
+use riscv::register::time;
+
+pub fn get_time() -> usize {
+    time::read()
+}
+
+pub fn get_time_ms() -> usize {
+    time::read() / CLOCK_FREQ * 1000
+}
+
+pub fn set_next_trigger() {
+    set_timer(get_time() + CLOCK_FREQ / TICKS_PER_SEC)
+}
