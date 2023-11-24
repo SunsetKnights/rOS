eaee4a46c73afb620240f870951e21ef26f9c6e0
SunsetKnights <sunsetknightss@gmail.com>
2023-11-24 21:04:50 +0800

    add some system call

--- OS-Core/src/syscall/mod.rs
+++ OS-Core/src/syscall/mod.rs
@@ -1,11 +1,16 @@
 const SYSCALL_WRITE: usize = 64;
 const SYSCALL_EXIT: usize = 93;
+const SYSCALL_YIELD: usize = 124;
+const SYSCALL_GET_TIME: usize = 169;
+const SYSCALL_TASK_INFO: usize = 410;
 
 mod fs;
 mod process;
 
 use fs::*;
 use process::*;
+
+use crate::task::info::TaskInfo;
 
 /// Call the real handler function by syscall id.
 ///
@@ -16,6 +21,9 @@ pub fn syscall(syscall_id: usize, args: [usize; 3]) -> isize {
     match syscall_id {
         SYSCALL_WRITE => sys_write(args[0], args[1] as *const u8, args[2]),
         SYSCALL_EXIT => sys_exit(args[0] as i32),
+        SYSCALL_YIELD => sys_yield(),
+        SYSCALL_GET_TIME => sys_get_time(),
+        SYSCALL_TASK_INFO => sys_task_info(args[0], args[1] as *mut TaskInfo),
         _ => panic!("Unsupported syscall_id: {}", syscall_id),
     }
 }
--- OS-Core/src/syscall/process.rs
+++ OS-Core/src/syscall/process.rs
@@ -1,9 +1,27 @@
 // process manage mod
 
-use crate::{batch::run_next_app, println};
+use crate::{
+    println,
+    task::{exit_current_and_run_next, info::TaskInfo, suspended_current_and_run_next},
+    timer::get_time_ms,
+};
 
 pub fn sys_exit(exit_code: i32) -> ! {
     println!("[kernel] Application exited with code {}", exit_code);
     // It may be necessary to delete the last UserContext saved in the kernel stack
-    run_next_app();
+    exit_current_and_run_next();
+    panic!("Unreachable in sys_exit!");
+}
+
+pub fn sys_yield() -> isize {
+    suspended_current_and_run_next();
+    0
+}
+
+pub fn sys_get_time() -> isize {
+    get_time_ms() as isize
+}
+
+pub fn sys_task_info(id: usize, ti: *mut TaskInfo) -> isize {
+    1
 }
