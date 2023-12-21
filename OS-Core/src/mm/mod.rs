de4d09cdd6bed8715f799cb51beba3bb76806dd2
SunsetKnights <sunsetknightss@gmail.com>
2023-12-21 18:51:16 +0800

    Memory management module

--- OS-Core/src/mm/mod.rs
+++ OS-Core/src/mm/mod.rs
@@ -1,1 +1,13 @@
+use self::memory_set::KERNEL_SPACE;
+
+pub mod address;
+pub mod frame_allocator;
 pub mod heap_allocator;
+pub mod memory_set;
+pub mod page_table;
+
+pub fn init() {
+    heap_allocator::init_heap();
+    frame_allocator::frame_allocator_init();
+    KERNEL_SPACE.exclusive_access().activate();
+}
