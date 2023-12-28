d65e54ee51158a0e684b21925ef9d74633830223
SunsetKnights <sunsetknightss@gmail.com>
2023-12-28 16:25:52 +0800

    User application for testing the kernel.

--- user/src/bin/04_tasks_info.rs
+++ user/src/bin/04_tasks_info.rs
@@ -1,18 +1,13 @@
 #![no_std]
 #![no_main]
 
-use user_lib::{get_task_info, get_time, yield_};
+use user_lib::get_task_info;
 
 #[macro_use]
 extern crate user_lib;
 
 #[no_mangle]
 fn main() -> i32 {
-    let current_timer = get_time();
-    let wait_for = current_timer + 5000;
-    while get_time() < wait_for {
-        yield_();
-    }
     for task_id in 0..=4 {
         let task_info = get_task_info(task_id);
         println!("{:#?}", task_info);
