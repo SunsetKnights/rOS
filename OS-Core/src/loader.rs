bb0783d0459b7a811fdbda77d3b685e0be5db6be
SunsetKnights <sunsetknightss@gmail.com>
2024-01-05 16:14:57 +0800

    Added function to read application name.

--- OS-Core/src/loader.rs
+++ OS-Core/src/loader.rs
@@ -1,3 +1,6 @@
+use alloc::vec::Vec;
+use lazy_static::lazy_static;
+
 pub fn get_num_app() -> usize {
     extern "C" {
         fn _num_app();
@@ -13,8 +16,9 @@ pub fn load_app(app_id: usize) -> &'static [u8] {
     //  .quad app_1_start
     //  .quad app_2_start
     //  .quad app_3_start
-    //  .quad app_4_start
-    //  .quad app_4_end
+    //  ......
+    //  .quad app_n_start
+    //  .quad app_n_end
     extern "C" {
         fn _num_app();
     }
@@ -27,4 +31,28 @@ pub fn load_app(app_id: usize) -> &'static [u8] {
             app_start[app_id + 1] - app_start[app_id],
         )
     }
+}
+
+lazy_static! {
+    static ref APP_NAMES: Vec<&'static str> = {
+        let num_app = get_num_app();
+        extern "C" {
+            fn _app_names();
+        }
+        let mut start = _app_names as usize as *const u8;
+        let mut result: Vec<&str> = Vec::new();
+        unsafe {
+            for _ in 0..num_app {
+                let mut end = start;
+                while end.read_volatile() != b'\0' {
+                    end = end.add(1);
+                }
+                let slice = core::slice::from_raw_parts(start, end as usize - start as usize);
+                let name_str = core::str::from_utf8(slice).unwrap();
+                result.push(name_str);
+                start = end.add(1);
+            }
+        }
+        result
+    };
 }
