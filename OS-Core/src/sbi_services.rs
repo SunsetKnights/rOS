4fac269a2072ea649b66a99fae59327a956815c1
SunsetKnights <sunsetknightss@gmail.com>
2024-01-17 17:26:54 +0800

    Added some sbi call.

--- OS-Core/src/sbi_services.rs
+++ OS-Core/src/sbi_services.rs
@@ -1,52 +1,60 @@
 #![allow(unused)]
 
-// sbi的一些服务的代号，即service_type
-const SBI_SET_TIMER: usize = 0;
-const SBI_CONSOLE_PUTCHAR: usize = 1;
-const SBI_CONSOLE_GETCHAR: usize = 2;
-const SBI_CLEAR_IPI: usize = 3;
-const SBI_SEND_IPI: usize = 4;
-const SBI_REMOTE_FENCE_I: usize = 5;
-const SBI_REMOTE_FENCE_VMA: usize = 6;
-const SBI_REMOTE_FENCE_VMA_ASID: usize = 7;
+// here is eid and fid for sbi call:
+// https://www.scs.stanford.edu/~zyedidia/docs/riscv/riscv-sbi.pdf
+const EID_SET_TIMER: usize = 0;
+const EID_CONSOLE_PUTCHAR: usize = 1;
+const EID_CONSOLE_GETCHAR: usize = 2;
+const FID_DEFAULT: usize = 0;
 
-const SRST_EXTENSION: usize = 0x53525354;
-const SBI_SHUTDOWN: usize = 0;
+const EID_SRST_EXTENSION: usize = 0x53525354;
+const FID_SHUTDOWN: usize = 0;
 
-use core::arch::asm;
+const EID_TIMER_EXTENSION: usize = 0x54494D45;
+const FID_SET_TIMER: usize = 0;
 
-use crate::timer;
+use core::{arch::asm, result};
+
+use crate::println;
 
 #[inline(always)]
-// 一个调用sbi服务的函数
-// service_type表示请求服务的i类型，arg0-arg2表示三个参数，返回值会被放入arg0中
-fn sbi_call(service_type: usize, fid: usize, arg0: usize, arg1: usize, arg2: usize) -> usize {
-    let mut result;
+fn sbi_call(extension_id: usize, function_id: usize, args: [usize; 6]) -> isize {
+    let result;
     unsafe {
         asm! {
             "ecall",
-            inlateout("x10") arg0=>result,
-            in("x11") arg1,
-            in("x12") arg2,
-            in("x16") fid,
-            in("x17") service_type,
+            inlateout("a0") args[0]=>result,
+            in("a1") args[1],
+            in("a2") args[2],
+            in("a3") args[3],
+            in("a4") args[4],
+            in("a5") args[5],
+            in("a6") function_id,
+            in("a7") extension_id,
         }
     }
     result
 }
 
-// 在屏幕上输出一个字符
+/// Put a char on screen.
 pub fn console_putchar(c: usize) {
-    sbi_call(SBI_CONSOLE_PUTCHAR, 0, c, 0, 0);
+    sbi_call(EID_CONSOLE_PUTCHAR, FID_DEFAULT, [c, 0, 0, 0, 0, 0]);
 }
 
-// 关机
+/// Get a char from console.
+pub fn console_getchar() -> u8 {
+    let result = sbi_call(EID_CONSOLE_GETCHAR, FID_DEFAULT, [0; 6]);
+    assert_ne!(result, -1, "Read char error from sbi.");
+    result as u8
+}
+
+/// Shutdown.
 pub fn shutdown() -> ! {
-    sbi_call(SRST_EXTENSION, SBI_SHUTDOWN, 0, 0, 0);
+    sbi_call(EID_SRST_EXTENSION, FID_SHUTDOWN, [0; 6]);
     panic!("shutdown command executed!");
 }
 
-/// set value of mtimecmp register
-pub fn set_timer(timer:usize){
-    sbi_rt::set_timer(timer as _);
+/// Set value of mtimecmp register.
+pub fn set_timer(timer: usize) {
+    sbi_call(EID_TIMER_EXTENSION, FID_SET_TIMER, [timer, 0, 0, 0, 0, 0]);
 }
