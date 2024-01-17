e44498e91358677e4e7205dd994084e4ae631861
SunsetKnights <sunsetknightss@gmail.com>
2024-01-17 17:34:43 +0800

    Added process management module.

--- OS-Core/src/main.rs
+++ OS-Core/src/main.rs
@@ -1,6 +1,5 @@
-#![no_std] //表示不用std，因为std需要system call，而是用core库
+#![no_std]
 #![no_main]
-//表示没有main函数，因为没有std库，所以也不存在main函数之前的初始化过程
 #![feature(panic_info_message)]
 #![feature(alloc_error_handler)]
 
@@ -9,12 +8,12 @@ extern crate bitflags;
 extern crate alloc;
 
 #[macro_use]
-mod lang_runtimes; //完成核心（core）库里面的一些功能，例如panic宏
+mod lang_runtimes;
 pub mod config;
-mod console; //提供屏幕打印的功能
+mod console;
 pub mod loader;
 pub mod mm;
-mod sbi_services; //提供调用sbi函数的功能
+mod sbi_services;
 pub mod sync;
 pub mod syscall;
 pub mod task;
@@ -27,14 +26,16 @@ global_asm!(include_str!("entry.asm"));
 global_asm!(include_str!("link_app.S"));
 
 #[allow(unused)]
-#[no_mangle] //告诉编译器不要乱改名字，不然entry.asm中找不到入口点
+#[no_mangle]
 pub fn rust_main() -> ! {
     clean_bss();
     mm::init();
     trap::init();
     trap::enable_timer_interrupt();
     timer::set_next_trigger();
-    task::run_first_task();
+    task::add_initproc();
+    loader::list_app();
+    task::processor::run_tasks();
     panic!("Unreachable in rust_main!");
 }
 
