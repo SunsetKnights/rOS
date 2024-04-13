0857b7c0ff6996d9164bf2ea7c9e0dcf4913218f
SunsetKnights <sunsetknightss@gmail.com>
2024-04-13 13:22:40 +0800

    Bug fixed.

--- easy-fs/src/lib.rs
+++ easy-fs/src/lib.rs
@@ -3,13 +3,13 @@ extern crate alloc;
 pub mod bitmap;
 pub mod block_cache;
 pub mod block_dev;
-pub mod layout;
 pub mod efs;
+pub mod layout;
 pub mod vfs;
 
 pub const BLOCK_SIZE: usize = 512;
 pub const MAX_BLOCK_CACHE_QUANTITY: usize = 16;
-pub const EFS_MAGIC: u32 = 114514;
+pub const EFS_MAGIC: u32 = 7604003;
 pub const INODE_DIRECT_COUNT: usize = 28;
 pub const INODE_INDIRECT_1_COUNT: usize = BLOCK_SIZE / 4;
 pub const INDIRECT_1_BOUND: usize = INODE_DIRECT_COUNT + INODE_INDIRECT_1_COUNT;
